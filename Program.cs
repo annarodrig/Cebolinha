b4e7f0bb318d369d8444736733eb904e6c612f0e
= <=>
2022-03-20 20:21:30 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string frasedigitada, frasecebolinha;
+
+Console.Write("Digite uma flase:");
+frasedigitada = Console.ReadLine();
+
+frasecebolinha = frasedigitada.Replace ("r", "l");
+
+Console.WriteLine($" {frasecebolinha}");
+
